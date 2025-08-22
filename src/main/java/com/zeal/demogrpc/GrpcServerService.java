6c3d79f68afd69130217489f433c2e715188c851
lambert <lambert.zhang@ychbluedigital.com>
2025-08-22 18:40:05 +0800

    first commit

---
+++ src/main/java/com/zeal/demogrpc/GrpcServerService.java
@@ -0,0 +1,40 @@
+package com.zeal.demogrpc;
+
+import io.grpc.stub.StreamObserver;
+import org.springframework.stereotype.Service;
+
+@Service
+class GrpcServerService extends SimpleGrpc.SimpleImplBase {
+
+
+    @Override
+    public void sayHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
+        if (req.getName().startsWith("error")) {
+            throw new IllegalArgumentException("Bad name: " + req.getName());
+        }
+        if (req.getName().startsWith("internal")) {
+            throw new RuntimeException();
+        }
+        HelloReply reply = HelloReply.newBuilder().setMessage("Hello ==> " + req.getName()).build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void streamHello(HelloRequest req, StreamObserver<HelloReply> responseObserver) {
+        int count = 0;
+        while (count < 10) {
+            HelloReply reply = HelloReply.newBuilder().setMessage("Hello(" + count + ") ==> " + req.getName()).build();
+            responseObserver.onNext(reply);
+            count++;
+            try {
+                Thread.sleep(1000L);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                responseObserver.onError(e);
+                return;
+            }
+        }
+        responseObserver.onCompleted();
+    }
+}
---
+++ src/test/java/com/zeal/demogrpc/DemoGrpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeal.demogrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGrpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
